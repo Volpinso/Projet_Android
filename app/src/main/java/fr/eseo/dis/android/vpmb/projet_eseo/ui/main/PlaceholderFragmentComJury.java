fa6e0286f97d831cf39d2804a7c2ded7e241cd2b
Barkallah <marwa.barkallah@reseau.eseo.fr>
2019-10-15 15:17:03 +0200

    checklist fichier a ajouter a un pseudo jury

--- app/src/main/java/fr/eseo/dis/android/vpmb/projet_eseo/ui/main/PlaceholderFragmentComJury.java
+++ app/src/main/java/fr/eseo/dis/android/vpmb/projet_eseo/ui/main/PlaceholderFragmentComJury.java
@@ -80,64 +80,64 @@ public class PlaceholderFragmentComJury extends Fragment {
             Bundle savedInstanceState) {
         context = getContext();
         View root = inflater.inflate(R.layout.fragment_com_member_jury, container, false);
+        if(AppDataBase.getINSTANCE(context).projectDAO().loadAll().isEmpty() ) {
+
+            //Return up to five subject non confidential
+            try {
+                // Instantiate the RequestQueue.
+                String token = LoginActivity.getToken();
+                RequestQueue queue = Volley.newRequestQueue(context);
+                String url = RequestModel.getRandomNonConfidProjetc(LoginActivity.getUsername(), token);
+                System.out.println(url);
+                // Request a string response from the provided URL.
+                JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+                            @Override
+                            public void onResponse(JSONObject response) {
+                                Gson gson = new Gson();
+                                Porte responseModel = gson.fromJson(String.valueOf(response),
+                                        Porte.class);
+                                for(int i = 0 ; i < responseModel.getProjects().length; i++){
+                                    projectList.add(responseModel.getProjects()[i]);
+                                }
+                                setProjectList(projectList);
+
+                                List<Project> projectToSave = DataConverter.convertProjectsToProject(projectList);
+                                if(!projectToSave.isEmpty()){
+                                    for(int i=0; i<projectToSave.size(); i++){
+                                        AppDataBase.getINSTANCE(context).projectDAO().insert(projectToSave.get(i));
+                                        System.out.println(AppDataBase.getINSTANCE(context).projectDAO().loadAll());
+
+                                    }
+                                }
+
+                            }
+                        }, new Response.ErrorListener() {
+
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                // TODO: Handle error
+                                projectList = null;
+                                System.out.println(error);
+
+                            }
+                        });
+
+                queue.add(jsonObjectRequest);
+                jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
+                        50000,
+                        DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                        DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
+                );
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
         Button buttonAllPfe = (Button) root.findViewById(R.id.buttonManualJury);
         buttonAllPfe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(AppDataBase.getINSTANCE(context).projectDAO().loadAll().isEmpty() ) {
-
-                    //Return up to five subject non confidential
-                    try {
-                        // Instantiate the RequestQueue.
-                        String token = LoginActivity.getToken();
-                        RequestQueue queue = Volley.newRequestQueue(v.getContext());
-                        String url = RequestModel.getRandomNonConfidProjetc(LoginActivity.getUsername(), token);
-                        System.out.println(url);
-                        // Request a string response from the provided URL.
-                        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
-                                (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
-
-                                    @Override
-                                    public void onResponse(JSONObject response) {
-                                        Gson gson = new Gson();
-                                        Porte responseModel = gson.fromJson(String.valueOf(response),
-                                                Porte.class);
-                                        for(int i = 0 ; i < responseModel.getProjects().length; i++){
-                                            projectList.add(responseModel.getProjects()[i]);
-                                        }
-                                        setProjectList(projectList);
-
-                                        List<Project> projectToSave = DataConverter.convertProjectsToProject(projectList);
-                                        if(!projectToSave.isEmpty()){
-                                            for(int i=0; i<projectToSave.size(); i++){
-                                                AppDataBase.getINSTANCE(context).projectDAO().insert(projectToSave.get(i));
-                                                System.out.println(AppDataBase.getINSTANCE(context).projectDAO().loadAll());
-
-                                            }
-                                        }
-
-                                    }
-                                }, new Response.ErrorListener() {
-
-                                    @Override
-                                    public void onErrorResponse(VolleyError error) {
-                                        // TODO: Handle error
-                                        projectList = null;
-                                        System.out.println(error);
-
-                                    }
-                                });
-
-                        queue.add(jsonObjectRequest);
-                        jsonObjectRequest.setRetryPolicy(new DefaultRetryPolicy(
-                                50000,
-                                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
-                                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT)
-                        );
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-            }
 
                 Intent intent = new Intent(getActivity(), CreatePseudoJuryManual.class);
                 startActivity(intent);
