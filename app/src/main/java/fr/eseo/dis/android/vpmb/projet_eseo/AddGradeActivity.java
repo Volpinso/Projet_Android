29f214c796d59c7917320741e24dc04c7b944d66
Petit Valentin <valentin.petit@reseau.eseo.fr>
2019-10-21 11:12:42 +0200

    activity add grade poster

--- app/src/main/java/fr/eseo/dis/android/vpmb/projet_eseo/AddGradeActivity.java
+++ app/src/main/java/fr/eseo/dis/android/vpmb/projet_eseo/AddGradeActivity.java
@@ -2,15 +2,28 @@ package fr.eseo.dis.android.vpmb.projet_eseo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import fr.eseo.dis.android.vp.projet_eseo.R;
+import fr.eseo.dis.android.vpmb.projet_eseo.ui.main.PlaceholderFragmentPfe;
 
 public class AddGradeActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Intent intent = getIntent();
+        int projectId = intent.getIntExtra("projectId", 1000);
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_grade);
+
+        TextView title = (TextView) findViewById(R.id.text_view_title_grade);
+        title.setText(PlaceholderFragmentPfe.getProjectList().get(projectId).getTitle());
+
+        TextView descriptionComment = (TextView) findViewById(R.id.text_view_description_grade);
+        descriptionComment.setText(PlaceholderFragmentPfe.getProjectList().get(projectId).getDescrip());
+
     }
 }
