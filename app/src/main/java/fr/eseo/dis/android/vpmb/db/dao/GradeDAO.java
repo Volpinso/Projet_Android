0b6af5dbf069cb6041004d8fb5e2718a6d360f48
Barkallah <marwa.barkallah@reseau.eseo.fr>
2019-10-21 12:02:42 +0200

    Noter/Commenter poster visiteur

--- app/src/main/java/fr/eseo/dis/android/vpmb/db/dao/GradeDAO.java
+++ app/src/main/java/fr/eseo/dis/android/vpmb/db/dao/GradeDAO.java
@@ -3,8 +3,12 @@ package fr.eseo.dis.android.vpmb.db.dao;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+import java.util.List;
 
 import fr.eseo.dis.android.vpmb.db.models.Grade;
+import fr.eseo.dis.android.vpmb.db.models.PseudoJury;
 
 @Dao
 public interface GradeDAO {
@@ -12,4 +16,6 @@ public interface GradeDAO {
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     public long insert(Grade grade);
 
+    @Query("SELECT * FROM Grade")
+    List<Grade> loadAll();
 }
--- app/src/main/java/fr/eseo/dis/android/vpmb/projet_eseo/GradeFormVisitor.java
+++ app/src/main/java/fr/eseo/dis/android/vpmb/projet_eseo/GradeFormVisitor.java
@@ -1,10 +1,22 @@
 package fr.eseo.dis.android.vpmb.projet_eseo;
 
+import androidx.annotation.StringRes;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.util.List;
 
 import fr.eseo.dis.android.vp.projet_eseo.R;
+import fr.eseo.dis.android.vpmb.db.AppDataBase;
+import fr.eseo.dis.android.vpmb.db.models.Grade;
 
 public class GradeFormVisitor extends AppCompatActivity {
 
@@ -12,5 +24,41 @@ public class GradeFormVisitor extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_grade_form_visitor);
+        setTitle(R.string.GradePosterVisitor);
+        final Spinner spinner = (Spinner) findViewById(R.id.grade_spinner);
+        final EditText edit = (EditText) findViewById(R.id.editText);
+        Button button = (Button) findViewById(R.id.button_grade);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                long idNotation=0;
+                List<Grade> grades = AppDataBase.getINSTANCE(GradeFormVisitor.this).gradeDAO().loadAll();
+                if(!grades.isEmpty()){
+                    idNotation=grades.size();
+                }
+
+                if (!"".equals(spinner.getSelectedItem().toString())){
+                    AppDataBase.getINSTANCE(GradeFormVisitor.this).gradeDAO().insert(new Grade(idNotation, Double.parseDouble(String.valueOf(spinner.getSelectedItemId())), edit.getText().toString(), ActivityVisitorListSubjects.getPseudoJuryVisitorId()));
+
+                    showNotationSucces(AppCompatActivity.RESULT_OK, getString(R.string.GradeSaved));
+                }
+            }
+        });
+
     }
+
+    private void showNotationSucces(@StringRes Integer successString, String message) {
+        String success = message;
+        Toast.makeText(getApplicationContext(), success, Toast.LENGTH_SHORT).show();
+        FragmentManager fm = getSupportFragmentManager();
+        if (fm.getBackStackEntryCount() > 0) {
+            Log.i("MainActivity", "popping backstack");
+            fm.popBackStack();
+        } else {
+            Log.i("MainActivity", "nothing on backstack, calling super");
+            super.onBackPressed();
+        }
+    }
+
 }
